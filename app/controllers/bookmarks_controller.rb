9aceb90a693985397d21919ee49e6b3ebc9bd304
Shamaila Sadaf <i>
2023-09-21 11:15:35 +0200

    done without stylsheet

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,39 @@
+# app/controllers/bookmarks_controller.rb
+
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = @list.bookmarks.build(bookmark_params)
+
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to @bookmark.list, notice: 'Bookmark was successfully deleted.'
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
